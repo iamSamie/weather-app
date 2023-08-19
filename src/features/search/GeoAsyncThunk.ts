08911729045ce44af84f815610f7ce50a77c25eb
Sam <>
2023-08-19 08:59:17 +0300

    поправил https

--- src/features/search/GeoAsyncThunk.ts
+++ src/features/search/GeoAsyncThunk.ts
@@ -1,11 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { GeolocationType } from '../../rtk/initialStateTypes'
 
-export const geoAsyncThunk = createAsyncThunk(
-    '@@geo',
-    async (sity: string) => {
+export const geoAsyncThunk = createAsyncThunk('@@geo', async (sity: string) => {
     const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${sity}&limit=5&appid=d36aa69487bec96f39ebe45d0ae17087`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${sity}&limit=5&appid=d36aa69487bec96f39ebe45d0ae17087`
     )
     return (await response.json()) as GeolocationType[]
 })
